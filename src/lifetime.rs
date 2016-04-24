6b56cdaa11cc951cb1613082298e4bd40f4f1903
ypyf <t34@qq.com>
2016-04-25 04:49:14 +0800

    add comments

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -10,7 +10,8 @@ fn failed_borrow<'a>() {
     let _x = 12;
 
     // ERROR: `_x` does not live long enough
-    let y: &i32 = &_x;
+    //let y: &'a i32 = &_x;
+    let y: &i32 = &_x; // it's ok
     // Attempting to use the lifetime `'a` as an explicit type annotation
     // inside the function will fail because the lifetime of `&_x` is shorter
     // than that of `y`. A short lifetime cannot be coerced into a longer one.
