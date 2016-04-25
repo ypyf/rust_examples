4e154ae4e90d32852dc9bfcc87ab8530195d9061
ypyf <t34@qq.com>
2016-04-25 23:46:21 +0800

    update Cargo.toml

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -1,3 +1,8 @@
+extern crate num;
+use num::{Num, Zero, One};
+extern crate core;
+use core::fmt::Display;
+
 // `print_refs` takes two references to `i32` which have different
 // lifetimes `'a` and `'b`. These two lifetimes must both be at
 // least as long as the function `print_refs`.
@@ -10,19 +15,26 @@ fn failed_borrow<'a>() {
     let _x = 12;
 
     // ERROR: `_x` does not live long enough
-    //let y: &'a i32 = &_x;
+    // let y: &'a i32 = &_x;
     let y: &i32 = &_x; // it's ok
     // Attempting to use the lifetime `'a` as an explicit type annotation
     // inside the function will fail because the lifetime of `&_x` is shorter
     // than that of `y`. A short lifetime cannot be coerced into a longer one.
 }
 
-fn foo<'a>(s: &'a str) {
-    println!("{}", s)
+fn foo<'a, T: Num + Display>(s: &'a str, n: T) {
+    println!("{} {}", s, n)
+    // for i in 0..n {
+    //     println!("{}", s)
+    // }
+}
+
+fn bar<'a>(x: &'a i32) -> &'a i32 {
+    return x;
 }
 
 fn main() {
-    foo("ssss");
+    foo::<i32>("ssss", 10);
     // Create variables to be borrowed below.
     let (four, nine) = (4, 9);
 
