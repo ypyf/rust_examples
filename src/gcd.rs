64f9de5d25e63e33531abe52153ada7df429da7a
ypyf <t34@qq.com>
2017-03-11 20:02:11 +0800

    fix gcd.rs

--- src/gcd.rs
+++ src/gcd.rs
@@ -1,16 +1,20 @@
 fn main() {
-    let m = 10;
-    let n = 2;
+    test(10, 2);
+    test(10, 3);
+    test(2, 10);
+    test(3, 10);
+}
+
+fn test(m: u64, n: u64) {
     println!("{}和{}的最大公约数是{}", m, n, gcd(m, n));
 }
 
 fn gcd(mut m: u64, mut n: u64) -> u64 {
     assert!(m > 0 && n > 0);
-    while m < n {
-        let t = m;
-        m = n;
-        n = t;
-        m = m % n;
+    while n != 0 {
+        let t = n;
+        n = m % n;
+        m = t;
     }
-    n
+    return m;
 }
